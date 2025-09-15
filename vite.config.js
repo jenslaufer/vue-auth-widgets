4a67a3eb95512bd90bca892dafb91f5dbf627ed3
Jens Laufer <jens.laufer@solytics.de>
2025-09-15 16:33:26 +0200

    added tailwindcss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tailwindcss(),
+    vue()],
   build: {
     lib: {
       entry: 'src/index.js',
