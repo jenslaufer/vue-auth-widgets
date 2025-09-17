f616a6918a9f7de161a62709c03d2e646411f7a8
Jens Laufer <jens.laufer@solytics.de>
2025-09-17 11:32:12 +0200

    changed

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   plugins: [
+    cssInjectedByJsPlugin(),
     tailwindcss(),
     vue()],
   build: {
