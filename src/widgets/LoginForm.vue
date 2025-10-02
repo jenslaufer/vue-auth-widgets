f0c65c545588d21d37f35a94bf8a76cec14588ba
Jens Laufer <jens.laufer@solytics.de>
2025-10-02 10:31:45 +0200

    fixed css class on input fields

--- src/widgets/LoginForm.vue
+++ src/widgets/LoginForm.vue
@@ -2,9 +2,9 @@
     <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
     <p v-if="errorMessage" :class="errorMessageClass">{{ errorMessage }}</p>
     <div class="mb-4">
-        <label for="email" :class="[`${inputClass}`,
-        $v.email.$error ? 'border-red-500 focus:ring-red-500' : 'border-gray-300 focus:ring-blue-500']">Email</label>
-        <input type="email" id="email" v-model="user.email" :class="inputClass" required>
+        <label for="email" :class="labelClass">Email</label>
+        <input type="email" id="email" v-model="user.email" :class="[`${inputClass}`,
+        v$.password.$errors ? 'border-red-500 focus:ring-red-500' : 'border-gray-300 focus:ring-blue-500']" required>
         <div v-if="$v.email.$errors.length" :class="errorMessageClass">
             <div v-for="error in $v.email.$errors" :key="error.$uid">
                 {{ error.$message }}
@@ -12,10 +12,9 @@
         </div>
     </div>
     <div class="mb-6">
-        <label for="password"
-            :class="[`${inputClass}`,
-            $v.password.$error ? 'border-red-500 focus:ring-red-500' : 'border-gray-300 focus:ring-blue-500']">Password</label>
-        <input type="password" id="password" v-model="user.password" :class="inputClass" required>
+        <label for="password" :class="labelClass">Password</label>
+        <input type="password" id="password" v-model="user.password" :class="[`${inputClass}`,
+        v$.password.$errors ? 'border-red-500 focus:ring-red-500' : 'border-gray-300 focus:ring-blue-500']" required>
         <div v-if="$v.password.$errors.length" :class="errorMessageClass">
             <div v-for="error in $v.password.$errors" :key="error.$uid">
                 {{ error.$message }}
