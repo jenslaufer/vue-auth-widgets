cd368a75f6f3fac36bcb637a541213af622ab8ae
Jens Laufer <jens.laufer@solytics.de>
2026-01-26 10:45:56 +0100

    upgrade

--- src/widgets/Registered.vue
+++ src/widgets/Registered.vue
@@ -81,7 +81,7 @@ onMounted(() => {
     const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
     if (key.value && uuidPattern.test(key.value)) {
         isUnregistered.value = false
-        emit(KEY_CHANGE_EMIT, key.value)
+        //emit(KEY_CHANGE_EMIT, key.value)
     } else {
         isUnregistered.value = true
     }
