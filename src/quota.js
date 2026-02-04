2a93961d2634d77974dd50ba819d17bc938b8c16
Jens Laufer <jens.laufer@solytics.de>
2026-02-04 10:24:31 +0100

    Added expiry

--- src/quota.js
+++ src/quota.js
@@ -1,28 +1,5 @@
 import { get, save } from "./storage.js";
-
-const secretKey = 'n8eneere88chbpbp8766bc97';
-
-// Simple encryption/decryption functions
-const encrypt = (value) => {// Consider using a more secure key
-    return btoa(String.fromCodePoint(...new TextEncoder().encode(JSON.stringify(value)).map((byte, i) =>
-        byte ^ secretKey.codePointAt(i % secretKey.length)
-    )));
-};
-
-const decrypt = (encryptedValue) => {
-    try {
-        const decrypted = new Uint8Array(
-            atob(encryptedValue).split('').map((char, i) =>
-                char.codePointAt(0) ^ secretKey.codePointAt(i % secretKey.length)
-            )
-        );
-        return JSON.parse(new TextDecoder().decode(decrypted));
-    } catch (error) {
-        // Return null for invalid encrypted data or JSON parsing errors
-        console.warn('Failed to decrypt data:', error.message);
-        return null;
-    }
-};
+import { encrypt, decrypt } from "./encryption.js";
 
 export const getQuotaUsed = async (quotaName) => {
     const encrypted = await get(quotaName);
